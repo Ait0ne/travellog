2cbe4075003ca17cd50ed48a8539bbca13363439
Ait0ne <bonafide112358@gmail.com>
2020-07-10 19:29:12 +0300

    change craco config

--- travellog/craco.config.js
+++ travellog/craco.config.js
@@ -2,10 +2,14 @@ module.exports = {
     webpack: {
       alias: {
         "react-dom": "@hot-loader/react-dom"
-      }
+      },
+      plugins: []
     },
     plugins: [
       { plugin: require("craco-plugin-react-hot-reload") },
       { plugin: require("craco-cesium")() }
-    ]
+    ],
+    options: {
+      loadPartially: true
+    }
   };
