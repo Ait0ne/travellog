1f376eb3b36306b1d62048dcb6ad8068ba8cd2a4
Ait0ne <bonafide112358@gmail.com>
2020-07-11 09:40:53 +0300

    some more changes to craco

--- travellog/craco.config.js
+++ travellog/craco.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     plugins: [
       { plugin: require("craco-plugin-react-hot-reload") },
-      { plugin: require("craco-cesium")({loadPartially:true, cesiumPath: '/cesium'}) }
+      { plugin: require("craco-cesium")({loadPartially:true, cesiumPath: ''}) }
     ],
 
   };
